3ec23f421e44a52c0d2aefeefc0342d6a8a52817
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2022-08-26 21:37:08 +0100

    ok

--- database/migrations/2022_08_03_135212_create_agent_groups_table.php
+++ database/migrations/2022_08_03_135212_create_agent_groups_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('agent_groups', function (Blueprint $table) {
             $table->id();
             $table->string('email');
+            $table->string('new')->default(0);
             $table->boolean('auto_assign')->default(0);
             $table->string('assign_method')->default('round_robin');
             $table->integer('counter')->nullable();
