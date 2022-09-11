b20d062d059262597a17f08b63cc58b3d1561604
Kevin Wilson <kevin.wilson@kevinlwilson.co.uk>
2022-09-11 17:54:26 +0100

    ok

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
 Route::get('/test', function () {
     return view('test');
 })->name('test');
